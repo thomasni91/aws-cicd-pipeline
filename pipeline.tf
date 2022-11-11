72e9ec01b0da910a681126b46f70b37bac2993d8
thomas ni <857840959@QQ.COM>
2022-11-11 16:56:46 +1100

    ss

--- pipeline.tf
+++ pipeline.tf
@@ -6,71 +6,17 @@ resource "aws_codebuild_project" "tf-plan" {
   artifacts {
     type = "CODEPIPELINE"
   }
-
-  #   environment {
-  #     compute_type                = "BUILD_GENERAL1_SMALL"
-  #     image                       = "hashicorp/terraform:0.14.3"
-  #     type                        = "LINUX_CONTAINER"
-  #     image_pull_credentials_type = "SERVICE_ROLE"
-  #     registry_credential {         
-  #       credential          = var.dockerhub_credentials
-  #       credential_provider = "SECRETS_MANAGER"
-  #     }
-  #   }
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/standard:1.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
-
-    # environment_variable {
-    #   name  = "SOME_KEY1"
-    #   value = "SOME_VALUE1"
-    # }
   }
   source {
     type      = "CODEPIPELINE"
     buildspec = file("buildspec/plan-buildspec.yml")
   }
 }
-
-resource "aws_codebuild_project" "tf-apply" {
-  name         = "tf-cicd-apply"
-  description  = "Apply stage for terraform"
-  service_role = aws_iam_role.tf-codebuild-role.arn
-
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-
-  #   environment {
-  #     compute_type                = "BUILD_GENERAL1_SMALL"
-  #     image                       = "hashicorp/terraform:0.14.3"
-  #     type                        = "LINUX_CONTAINER"
-  #     image_pull_credentials_type = "SERVICE_ROLE"
-  #     registry_credential {
-  #       credential          = var.dockerhub_credentials
-  #       credential_provider = "SECRETS_MANAGER"
-  #     }
-  #   }
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:1.0"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-
-    # environment_variable {
-    #   name  = "SOME_KEY1"
-    #   value = "SOME_VALUE1"
-    # }
-  }
-  source {
-    type      = "CODEPIPELINE"
-    buildspec = file("buildspec/apply-buildspec.yml")
-  }
-}
-
-
 resource "aws_codepipeline" "cicd_pipeline" {
 
   name     = "tf-cicd"
@@ -92,11 +38,10 @@ resource "aws_codepipeline" "cicd_pipeline" {
         output_artifacts = ["tf-code"]
     #   output_artifacts = ["source_output"]
       configuration = {
-        FullRepositoryId = "thomasni91/aws-cicd-pipeline"
+        FullRepositoryId = "thomasni91/quickfront"
         BranchName       = "master"
         # ConnectionArn        = var.codestar_connector_credentials
         ConnectionArn        = aws_codestarconnections_connection.example.arn
-
         OutputArtifactFormat = "CODE_ZIP"
       }
     }
@@ -117,22 +62,6 @@ resource "aws_codepipeline" "cicd_pipeline" {
       }
     }
   }
-
-  stage {
-    name = "good"
-    action {
-      name            = "Deploy"
-      category        = "Build"
-      provider        = "CodeBuild"
-      version         = "1"
-      owner           = "AWS"
-      input_artifacts = ["tf-code"]
-      configuration = {
-        ProjectName = "tf-cicd-apply"
-      }
-    }
-  }
-
 }
 
 resource "aws_codestarconnections_connection" "example" {
